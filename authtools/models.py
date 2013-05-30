209a2a465789130759281d8bc02e4c9e58c1a882
Gavin Wahl <gwahl@fusionbox.com>
2013-05-30 13:53:50 -0600

    [0.1.x] Call normalize_email on an instance, not a class.
    see https://github.com/django/django/pull/1232
    
    Backport of 946f42c9936d0df206b43c98fb15d248a2b09703 from master.

--- authtools/models.py
+++ authtools/models.py
@@ -10,7 +10,7 @@ from django.contrib.auth.models import BaseUserManager
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **kwargs):
-        email = UserManager.normalize_email(email)
+        email = self.normalize_email(email)
         user = self.model(email=email, **kwargs)
         user.set_password(password)
         user.save(using=self._db)
