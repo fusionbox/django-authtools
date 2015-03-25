c367c8a8ced8ff3d9ef1a894fc9ce522790c2e77
Bradley Gordon <bgordon@fusionbox.com>
2015-03-25 14:01:04 -0600

    Add a new authentication backend for case-insensitive emails as a possible solution for issue #11

---
+++ authtools/backends.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.backends import ModelBackend
+
+
+class CaseInsensitiveEmailBackend(ModelBackend):
+    """
+    This authentication backend assumes that usernames are email addresses and simply lowercases
+    a username before an attempt is made to authenticate said username using Django's ModelBackend.
+
+    Example usage:
+        # In settings.py
+        AUTHENTICATION_BACKENDS = ('authtools.backends.CaseInsensitiveEmailBackend',)
+    """
+    def authenticate(self, username=None, password=None):
+        username = username.lower()
+
+        return super(CaseInsensitiveEmailBackend, self).authenticate(
+            username=username,
+            password=password
+        )
