999786e8027cd39bc1ee29e13614f3e84c80769e
Bradley Gordon <bgordon@fusionbox.com>
2015-03-26 08:25:07 -0600

    Add cautionary message to potential users of the backend

--- authtools/backends.py
+++ authtools/backends.py
@@ -9,6 +9,11 @@ class CaseInsensitiveEmailBackend(ModelBackend):
     Example usage:
         # In settings.py
         AUTHENTICATION_BACKENDS = ('authtools.backends.CaseInsensitiveEmailBackend',)
+
+    NOTE:
+        A word of caution. Use of this backend presupposes a way to ensure that users cannot create
+        usernames that differ only in case (e.g., joe@test.org and JOE@test.org). Using this backend
+        in such a system is a huge security risk.
     """
     def authenticate(self, username=None, password=None, **kwargs):
         if username is not None:
