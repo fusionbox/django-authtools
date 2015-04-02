23ec0899eaf60a9dc79f6671461a33eea7e7f464
Bradley Gordon <bgordon@fusionbox.com>
2015-04-02 09:22:09 -0600

    Add mixin to make the case-insensitive email auth backend more flexible

--- authtools/backends.py
+++ authtools/backends.py
@@ -1,6 +1,18 @@
 from django.contrib.auth.backends import ModelBackend
 
 
+class CaseInsensitiveEmailBackendMixin(object):
+    def authenticate(self, username=None, password=None, **kwargs):
+        if username is not None:
+            username = username.lower()
+
+        return super(CaseInsensitiveEmailBackendMixin, self).authenticate(
+            username=username,
+            password=password,
+            **kwargs
+        )
+
+
 class CaseInsensitiveEmailBackend(ModelBackend):
     """
     This authentication backend assumes that usernames are email addresses and simply lowercases
