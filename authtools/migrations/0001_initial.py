203056c869d43c2cd5fcd9325c7ad8c0595dc376
Basil Shubin <basil.shubin@gmail.com>
2015-08-09 15:19:33 +0500

    fix tests

--- authtools/migrations/0001_initial.py
+++ authtools/migrations/0001_initial.py
@@ -8,7 +8,7 @@ import django.utils.timezone
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('auth', '0001_initial'),
+        ('auth', '__first__'),
     ]
 
     operations = [
