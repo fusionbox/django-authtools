f27c6341e4d6e5b294feeede4ae1fc67324caf04
Antoine Catton <acatton@fusionbox.com>
2015-09-22 16:29:39 -0600

    Use callable for urlpatterns
    The use of string is deprecated and will be removed in Django 1.10

--- authtools/urls.py
+++ authtools/urls.py
@@ -1,28 +1,19 @@
-import django
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+
+from authtools import views
 
 
-urlpatterns = patterns('authtools.views',
-    url(r'^login/$', 'login', name='login'),
-    url(r'^logout/$', 'logout', name='logout'),
-    url(r'^password_change/$', 'password_change', name='password_change'),
-    url(r'^password_change/done/$', 'password_change_done', name='password_change_done'),
-    url(r'^password_reset/$', 'password_reset', name='password_reset'),
-    url(r'^password_reset/done/$', 'password_reset_done', name='password_reset_done'),
-    url(r'^reset/done/$', 'password_reset_complete', name='password_reset_complete'),
-)
-
-if django.VERSION < (1, 6):
-    urlpatterns += patterns('authtools.views',
-        url(r'^reset/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-            'password_reset_confirm',
-            name='password_reset_confirm'),
-    )
-else:
-    urlpatterns += patterns('authtools.views',
-        url(r'^reset/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-            'password_reset_confirm_uidb36'),
-        url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-            'password_reset_confirm',
-            name='password_reset_confirm'),
-    )
+urlpatterns = [
+    url(r'^login/$', views.LoginView.as_view(), name='login'),
+    url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
+    url(r'^password_change/$', views.PasswordChangeView.as_view(), name='password_change'),
+    url(r'^password_change/done/$', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    url(r'^password_reset/$', views.PasswordResetView.as_view(), name='password_reset'),
+    url(r'^password_reset/done/$', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    url(r'^reset/done/$', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    url(r'^reset/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.PasswordResetConfirmView.as_view()),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'),
+]
--- tests/tests/urls.py
+++ tests/tests/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib import admin
+from django.core.urlresolvers import reverse_lazy
 
-from authtools.views import PasswordResetView
+from authtools import views
 from authtools.forms import FriendlyPasswordResetForm
 
 admin.autodiscover()
@@ -14,22 +15,14 @@ def dumbview(request):
     return HttpResponse('dumbview')
 
 
-if django.VERSION < (1, 6):
-    urlpatterns = patterns('authtools.views',
-        url('^reset_and_login/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', 'password_reset_confirm_and_login'),
-    )
-else:
-    urlpatterns = patterns('authtools.views',
-        url(r'^reset_and_login/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', 'password_reset_confirm_and_login'),
-    )
-
-urlpatterns += patterns('',
-    url('^logout-then-login/$', 'authtools.views.logout_then_login', name='logout_then_login'),
-    url('^friendly_password_reset/$',
-        PasswordResetView.as_view(form_class=FriendlyPasswordResetForm),
+urlpatterns = [
+    url(r'^reset_and_login/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.PasswordResetConfirmAndLoginView.as_view()),
+    url(r'^logout-then-login/$', views.LogoutView.as_view(url=reverse_lazy('login')), name='logout_then_login'),
+    url(r'^friendly_password_reset/$',
+        views.PasswordResetView.as_view(form_class=FriendlyPasswordResetForm),
         name='friendly_password_reset'),
-    url('^login_required/$', login_required(dumbview), name='login_required'),
+    url(r'^login_required/$', login_required(dumbview), name='login_required'),
     # From django.contrib.auth.tests.url
-    url('^password_reset/html_email_template/$', 'django.contrib.auth.views.password_reset', dict(html_email_template_name='registration/html_password_reset_email.html')),
-    url('^', include('authtools.urls')),
-)
+    url(r'^password_reset/html_email_template/$', views.PasswordResetView.as_view(html_email_template_name='registration/html_password_reset_email.html')),
+    url(r'^', include('authtools.urls')),
+]
