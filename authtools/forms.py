c206c62fbb714cbcafbbabb684d3575aaf7e7f47
Gavin Wahl <gwahl@fusionbox.com>
2015-09-29 17:38:32 -0600

    link to password form correctly

--- authtools/forms.py
+++ authtools/forms.py
@@ -154,7 +154,7 @@ class AdminUserChangeForm(UserChangeForm):
             self.fields['password'].help_text = _(
                 "Raw passwords are not stored, so there is no way to see this"
                 " user's password, but you can change the password using"
-                " <a href=\"password/\">this form</a>.")
+                " <a href=\"../password/\">this form</a>.")
 
 
 class FriendlyPasswordResetForm(OldPasswordResetForm):
