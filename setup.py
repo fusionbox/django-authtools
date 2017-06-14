4c688a17c1db204f56cfa7247a5028c12a6c4c97
Alexander Bliskovsky <abliskovsky@fusionbox.com>
2017-06-14 10:01:21 -0600

    Reorder imports

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
-import os
 import io
+import os
+
+from setuptools import setup, find_packages
 
 __doc__ = ("Custom user model app for Django featuring email as username and"
            " class-based views for authentication.")
