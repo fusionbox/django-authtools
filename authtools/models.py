b6ed0276d68d81e84e433e24dcbc20e9db982acc
sensus261 <44163811+sensus261@users.noreply.github.com>
2019-12-06 14:29:45 +0200

    Update models.py
    Switched django.utils.encoding.python_2_unicode_compatible to six.python_2_unicode_compatible

--- authtools/models.py
+++ authtools/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.core.mail import send_mail
 from django.db import models
 from django.utils import timezone
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 
