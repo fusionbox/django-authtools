e4e41c7e250c557fa7f002f5ff7ef8bdf65fdc0f
mplanes <mplanes@users.noreply.github.com>
2020-01-24 12:22:20 +0100

    Update models.py

--- authtools/models.py
+++ authtools/models.py
@@ -4,7 +4,11 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.core.mail import send_mail
 from django.db import models
 from django.utils import timezone
-from six import python_2_unicode_compatible
+from django import VERSION as DJANGO_VERSION
+if DJANGO_VERSION[0] < 3:
+    from django.utils.encoding import python_2_unicode_compatible
+else:
+    from six import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 
