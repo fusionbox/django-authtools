361493e1ba2b510265314d70c63ec2a13d06b208
bgaudino <bgaudino@gmail.com>
2024-03-18 14:19:48 -0500

    Handle python < 3.8

--- authtools/__init__.py
+++ authtools/__init__.py
@@ -1,3 +1,8 @@
-from importlib.metadata import version
+try:
+    from importlib.metadata import version
 
-__version__ = version('django-authtools')
+    __version__ = version('django-authtools')
+except ImportError:
+    import pkg_resources
+
+    __version__ = pkg_resources.get_distribution('django-authtools').version
