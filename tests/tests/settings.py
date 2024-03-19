7e999758b987a922083a49560b3c170b1f02c7a0
bgaudino <bgaudino@gmail.com>
2024-03-19 09:25:21 -0500

    Make hasher used in tests explicit

--- tests/tests/settings.py
+++ tests/tests/settings.py
@@ -60,9 +60,6 @@ TEMPLATES = [
 
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-    'django.contrib.auth.hashers.BCryptPasswordHasher',
 ]
 
 USE_TZ = False
