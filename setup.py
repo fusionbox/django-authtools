eecc4903f26a9f0ef1d7f772096c51e0825609eb
bgaudino <bgaudino@gmail.com>
2024-03-19 13:47:10 -0500

    Back to development: 2.0.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = [
 
 setup(
     name='django-authtools',
-    version='2.0.1',
+    version='2.0.2.dev0',
     author='Fusionbox, Inc.',
     author_email='programmers@fusionbox.com',
     description=__doc__,
